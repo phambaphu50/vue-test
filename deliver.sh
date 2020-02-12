31c66f0483b2caf39c299bb556fe9ea672052b92
phu pham <phu.pham@mail.i-on.net>
2020-02-12 14:28:56 +0700

    update jenkinfile

--- deliver.sh
+++ deliver.sh
@@ -8,7 +8,7 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm run dev &
+npm start &
 sleep 1
 echo $! > .pidfile
 set +x
